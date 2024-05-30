2572fc9576552860ee7497ecd3ca77e36e22faa8
kwiats <pawuskwiatkowski@gmail.com>
2024-05-30 15:00:57 +0200

    [pk] - added config

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+TORCH_DEVICE = os.getenv('TORCH_DEVICE')
+RECORDER_FPS = int(os.getenv('RECORDER_FPS', 30))
+QUEUE_MAX_SIZE = int(os.getenv('QUEUE_MAX_SIZE', 100))
+YOLO_MODEL_PATH = os.getenv('MODEL_PATH', 'yolov10n.pt')
+OUTPUT_FILE = os.getenv('OUTPUT_FILE', 'output.mp4')
+DISPLAY_SCREEN = os.getenv('DISPLAY_SCREEN', 'n').lower() == 'y'
+LIST_WINDOWS = os.getenv('LIST_WINDOWS', 'n').lower() == 'y'
+APP_NAME = os.getenv('APP_NAME')
+VIDEO_FORMAT = os.getenv('VIDEO_FORMAT', 'mp4v')
+DETECTED_SCREEN_NAME = os.getenv('DETECTED_SCREEN_NAME', 'Detected Frame')
